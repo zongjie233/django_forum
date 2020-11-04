d571ee7c3f29364eb6b90b4c38bf61a8b1695d29
zongjie233 <840573195@qq.com>
2020-11-04 22:13:34 +0800

    代码重构

--- djangoforum/settings.py
+++ djangoforum/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'boards',
     'widget_tweaks',
-    'django.contrib.humanize',
     'accounts',
 ]
 
